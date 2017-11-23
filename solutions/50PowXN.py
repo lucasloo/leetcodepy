f91f34de78d8e50a20b9932a03d0c8c849dc978d
lucasloo <bupt_lucas@foxmail.com>
2017-11-23 16:10:53 +0800

    50PowXN

---
+++ solutions/50PowXN.py
@@ -0,0 +1,69 @@
+# Implement pow(x, n).
+
+# Example 1:
+
+# Input: 2.00000, 10
+# Output: 1024.00000
+
+# Example 2:
+
+# Input: 2.10000, 3
+# Output: 9.26100
+
+class Solution:
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x ** n
+
+# after reading the discuss part, I realize that this problems is about solving different situation when it comes to pow. Belows are different approach.
+double myPow(double x, int n) {
+    if(n<0) return 1/x * myPow(1/x, -(n+1));
+    if(n==0) return 1;
+    if(n==2) return x*x;
+    if(n%2==0) return myPow( myPow(x, n/2), 2);
+    else return x*myPow( myPow(x, n/2), 2);
+}
+
+    double myPow
+
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    double t = myPow(x,n/2);
+    if(n%2) return n<0 ? 1/x*t*t : x*t*t;
+    else return t*t;
+}
+
+    double x
+
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    if(n<0){
+        n = -n;
+        x = 1/x;
+    }
+    return n%2==0 ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+}
+
+    iterative one
+
+double myPow(double x, int n) { 
+    if(n==0) return 1;
+    if(n<0) {
+        n = -n;
+        x = 1/x;
+    }
+    double ans = 1;
+    while(n>0){
+        if(n&1) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+    bit operation
+
