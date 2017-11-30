84034a472585981a9d5f8380e5f7ff3be458c49f
lucasloo <bupt_lucas@foxmail.com>
2017-11-30 16:56:19 +0800

    71SimplifyPath

---
+++ solutions/71SimplifyPath.py
@@ -0,0 +1,36 @@
+# Given an absolute path for a file (Unix-style), simplify it.
+
+# For example,
+# path = "/home/", => "/home"
+# path = "/a/./b/../../c/", => "/c"
+
+# click to show corner cases.
+# Corner Cases:
+
+#     Did you consider the case where path = "/../"?
+#     In this case, you should return "/".
+#     Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+#     In this case, you should ignore redundant slashes and return "/home/foo".
+
+
+import re
+class Solution:
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        l = re.split(r"/+", path)
+        stack = []
+        for i in l:
+            if i == "" or i == ".":
+                continue
+            if i == "..":
+                if len(stack):
+                    stack.pop()
+            else:
+                stack.append(i)
+        res = ""
+        for i in stack:
+            res += "/" + i
+        return res if res else "/"
