4ba726b96b1e3193bc46a0a4bf5e04f48fcfc3e6
lucasloo <bupt_lucas@foxmail.com>
2017-12-05 15:59:38 +0800

    33SearchInRotatedSortedArray

--- solutions/33SearchInRotatedSortedArray.py
+++ solutions/33SearchInRotatedSortedArray.py
@@ -25,3 +25,21 @@ class Solution:
             if nums[tmp] == target:
                 return tmp
         return -1
+
+
+# binary search
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if (nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]):
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo if target in nums[lo:lo+1] else -1
